3a4772ee5de6f877a6555d7afd7e2f2c86d4b76c
Diego Souza <diegoshowza93@gmail.com>
2020-10-25 21:02:59 -0300

    adding establishment, rating and store services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import Establishment from './components/Establishment';
+
 import EstablishmentsService from './services/establishment_service';
 
 const App = () => {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
   const [locations, setLocations] = useState([]);
+  const [selected, setSelected] = useState({});
   
   const { REACT_APP_GOOGLE_API_KEY } = process.env;
 
@@ -32,16 +35,22 @@ const App = () => {
     <Fragment>
       <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_API_KEY}>
         <GoogleMap mapContainerStyle={{ height: "100vh", width: "100%" }}
-          zoom={15}
+          zoom={13}
           center={{ lat: latitude, lng: longitude }}>
           {
             locations.map((item, index) => {
               return (
                 <Marker key={index} icon="/images/coffee-pin.png" title={item.name} animation="4"
                   position={{ lat: item.geometry.location.lat, lng: item.geometry.location.lng }}
+                  onClick={() => setSelected(item)}
                 />
               )
             })
+          }
+          {
+            selected.place_id && (
+              <Establishment place={selected} />
+            )
           }
           <Marker key="my location" icon="/images/my-location-pin.png" title="Seu local" animation="2"
             position={{ lat: latitude, lng: longitude }}
---
+++ src/components/Establishment/Ratings/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import Form from './Form';
+
+const Ratings = (props) => {
+  return (
+    <Fragment>
+      <Form place={props.place} />
+    </Fragment>
+  )
+}
+
+export default Ratings;
